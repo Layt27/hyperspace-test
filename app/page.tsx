8e372f7706467c45ea6024094bb0d2e0bbb9b8f1
Layt27 <laytonchetty@gmail.com>
2024-06-01 13:23:06 +0400

    refactor: Design home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 // Imports
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
-	return (
-		<main>
-			<h1>Hyperspace Test</h1>
-			<div>
-				<Link href="/page1"><button>Click to go to page 1</button></Link>
-			</div>
-
-			<div>
-				<Link href="/page2"><button>Click to go to page 2</button></Link>
-			</div>
-		</main>
+  	return (
+    	<main className='flex flex-col items-center justify-center min-h-screen'>
+      	<h1 className='text-5xl mb-8'>Hyperspace Test</h1>
+      	<div className='flex justify-center w-full space-x-16'>
+        	<div className='flex flex-col items-start'>
+          		<Link href='/page1'>
+            		<button className='bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md'>Click to go to page 1</button>
+          		</Link>
+        	</div>
+        	<div className='flex flex-col items-end'>
+          		<Link href='/page2'>
+            		<button className='bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md'>Click to go to page 2</button>
+          		</Link>
+        	</div>
+      	</div>
+    	</main>
   	);
-};
+}
