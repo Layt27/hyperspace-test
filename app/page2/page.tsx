774f734d62d67344aae05672591f0ee7bc467729
Layt27 <laytonchetty@gmail.com>
2024-06-02 13:55:46 +0400

    feature: Add rotating box along x-axis

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -5,16 +5,16 @@ import MotionContainer from '@/components/shared/MotionContainer'
 
 const page2 = () => {
     return (
-        <div className="flex flex-col items-center">
-            <div className="bg-pink-700 w-24 px-10 py-10 rounded-md">
-                <MotionContainer
-                    initial={{ scale: 0 }}
-                    animate={{ x: 200, scale: 2 }}
-                    transition={{ duration: 2, repeat: Infinity }}
-                >
-                    hello
-                </MotionContainer>
-            </div>
+        <div className="flex flex-col items-center justify-center min-h-80">
+            <MotionContainer
+                className="bg-violet-300 w-24 px-10 py-10 rounded-md mb-16"
+                animate={{
+                    rotate: [0, 320, 320, 0],
+                    x: [0, 400, 400, 0, 0, -400, -400, 0],
+                }}
+                transition={{ duration: 1.3, repeat: Infinity }}
+                children={undefined}
+            ></MotionContainer>
             <div className="flex justify-center items-center">
                 <Link href="/">
                     <button className="bg-blue-500 hover:bg-blue-800 text-white px-4 py-2 rounded-md">
